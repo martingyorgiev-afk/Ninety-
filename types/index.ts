1c78782f12eaece72caa367aeeb463c23fcc348e
martingyorgiev-afk <martingyorgiev@gmail.com>
2026-08-27 07:09:42 +0200

    Add TodayData types

---
+++ types/index.ts
@@ -0,0 +1,9 @@
+export type TodayData = {
+  currentWeight: number;
+  targetWeight: number;
+  calories: number;
+  protein: number;
+  water: number;
+  readiness: number;
+  sleepHours: number;
+};
